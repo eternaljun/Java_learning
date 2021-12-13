c0e0b204c79c9ff58605ec821eb28603f9e4bb6a
Anatoly <darcstile@mail.ru>
2021-12-13 17:30:09 +0700

    Chapter 12

---
+++ src/part1/Chapter12/EnumDemo2.java
@@ -0,0 +1,22 @@
+package part1.Chapter12;
+
+public class EnumDemo2 {
+    public static void main(String[] args) {
+        Apple ap;
+
+        System.out.println("Константы перечисляемого типа Apple:");
+
+        // применить метод values()
+        Apple[] allapples = Apple.values();
+        for (Apple a : allapples) {
+            System.out.println(a);
+        }
+        System.out.println();
+
+        // применить метод valueOf()
+        ap = Apple.valueOf("Winesap");
+        System.out.println("Переменная ap содержит " + ap);
+
+    }
+}
+
